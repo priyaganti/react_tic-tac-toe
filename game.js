49301b7c0c41d72f855e867b8407bf144d6c3621
Priyanka Ganti <ganti.priya@gmail.com>
2017-01-13 13:27:23 -0800

    add board component which calls square component

--- game.js
+++ game.js
@@ -5,8 +5,41 @@
  */
 
 /* Square Component */
-class Square renders React.Component{
+class Square extends React.Component{
   render(){
     return (<button className="square"></button>);
+  }
+}
+
+/* Board Component */
+/* Create a board with 3 rows and let each column call the _renderSquare function
+ * to display the button from Square component.
+ */
+class Board extends React.Component{
+  _renderSquare(i){
+    return <Square />
+  }
+  render(){
+    const status = 'Next Player: X';
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this._renderSquare(0)}
+          {this._renderSquare(1)}
+          {this._renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this._renderSquare(3)}
+          {this._renderSquare(4)}
+          {this._renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this._renderSquare(6)}
+          {this._renderSquare(7)}
+          {this._renderSquare(8)}
+        </div>
+      </div>
+    );
   }
 }
